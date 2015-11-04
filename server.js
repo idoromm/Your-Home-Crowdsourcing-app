1c4b77d1ea55e0261ea284bec9ece9f727238668
stiv1qaz1 <stiv1qaz1@gmail.com>
2015-11-04 23:39:10 +0200

    Google Maps Integration

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3000);
+
+console.log("Running at Port 3000");

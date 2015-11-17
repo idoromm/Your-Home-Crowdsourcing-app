ce68ba24e45f38865ba20db11abdcdcf84f8a350
Ido <idoromm@gmail.com>
2015-11-17 16:48:52 +0200

    create user schema and fixes to listing page + typos

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path            = require("path");
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
 var mongoose        = require('mongoose');
-var app             = express(); //express faramwork
+var app             = express(); // express framework
 
 //passportJs set up ==================================
 
@@ -28,7 +28,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 //print first time db opened
 db.once('open', function (callback) {
-  console.log("Loging in to the DB...")
+  console.log("Logging in to the DB...")
 });
 
 

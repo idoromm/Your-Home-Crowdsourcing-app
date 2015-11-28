6537e7764eb606014e1d821cbf06ef8e82d8f873
Ido <idoromm@gmail.com>
2015-11-28 16:33:23 +0200

    Added flag as spam button. Added ListingController to appRoutes.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,9 @@
 // navigation slide-in
-$(window).load(function() {
-  $('.nav_slide_button').click(function() {
-    $('.pull').slideToggle();
-  });
+$(window).load(function () {
+    $('.nav_slide_button').click(function () {
+        $('.pull').slideToggle();
+    });
 });
+function raiseAlert() {
+    swal("Thank you!", "This listing has been reported", "success")
+}

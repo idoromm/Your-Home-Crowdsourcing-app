465a144ad5d957f6687856b8f1f410bbfaf83bd4
Ido <idoromm@gmail.com>
2015-11-28 21:55:08 +0200

    added ListingCtrl.js controller for listings obv! changes in listing page (single.html), including adding imports for angularjs code removed stuff i believe we didn't need from app.js (left it very minimalistic)

---
+++ public/js/controllers/ListingCtrl.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Ido on 28/11/2015.
+ */
+
+(function() {
+    angular.module('app', []).controller('ListingCtrl', function($scope) {
+    /*    $scope.data = {
+            Colors: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        } */
+        var vm = this;
+        vm.title = 'YOUR HOME';
+    });
+}());

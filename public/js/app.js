0dbe21ed7b0e275a45c5a83cd8b17c3d6bcd587a
liorgluskin <lior.gluskin@vayyar.com>
2015-11-29 23:08:58 +0200

    added partial support for showing name in navigation

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,8 @@
 
-angular.module('app', ['']);
+var app = angular.module('app', []);
+
+app.controller('NavCtrl', function ($scope, $http) {
+	$http.get('/api/user').success(function (response) {
+		$scope.User = response;
+	});
+});
---
+++ public/js/controllers/navCtrl.js
@@ -0,0 +1,10 @@
+﻿/**
+ * Created by Lior on 29/11/2015.
+ */
+
+var app = angular.module('app', []);
+
+
+app.controller('NavCtrl', function ($scope) {
+	this.message = 'lior';
+});

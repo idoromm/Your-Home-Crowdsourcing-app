0d40596bd52fb1bc4334da3013fb0f741f469746
stiv1qaz1 <stiv1qaz1@gmail.com>
2015-12-02 23:37:45 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 	//set your google maps parameters
 	var latitude = 32.075530,
 		longitude = 34.775082,
-		map_zoom = 18;
+		map_zoom = 15;
 
 	//google map custom marker icon - .png fallback for IE11
 	var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
@@ -228,13 +228,66 @@ jQuery(document).ready(function($){
 
   	//insert the zoom div on the top left of the map
   	map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
-	google.maps.event.addDomListener(window, 'load', initialize);
+	google.maps.event.addDomListener(window, 'load', function initialize() {
+
+		var input = document.getElementById('searchTextField');
+		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+		console.log("input: "+input);
+		var autocomplete = new google.maps.places.Autocomplete(input);
+
+
+		var infowindow = new google.maps.InfoWindow();
+		var marker = new google.maps.Marker({
+			map: map,
+			anchorPoint: new google.maps.Point(0, -29)
+		});
+
+
+		autocomplete.addListener('place_changed', function() {
+			infowindow.close();
+			marker.setVisible(false);
+			var place = autocomplete.getPlace();
+			if (!place.geometry) {
+				window.alert("Autocomplete's returned place contains no geometry");
+				return;
+			}
+
+			// If the place has a geometry, then present it on a map.
+			if (place.geometry.viewport) {
+				map.fitBounds(place.geometry.viewport);
+			} else {
+				map.setCenter(place.geometry.location);
+				map.setZoom(17);  // Why 17? Because it looks good.
+			}
+			marker.setIcon(/** @type {google.maps.Icon} */({
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(35, 35)
+			}));
+			marker.setPosition(place.geometry.location);
+			marker.setVisible(true);
+
+			var address = '';
+			if (place.address_components) {
+				address = [
+					(place.address_components[0] && place.address_components[0].short_name || ''),
+					(place.address_components[1] && place.address_components[1].short_name || ''),
+					(place.address_components[2] && place.address_components[2].short_name || '')
+				].join(' ');
+			}
+
+			infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+			infowindow.open(map, marker);
+		});
+
+
+
+	});
 });
 
 
-function initialize() {
 
-	var input = document.getElementById('searchTextField');
-	var autocomplete = new google.maps.places.Autocomplete(input);
-}
 

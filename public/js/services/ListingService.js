6d70a45066763d0532b3ba00caf2e913797f103d
Ido <idoromm@users.noreply.github.com>
2015-12-04 22:32:23 +0200

    fix some issues

--- public/js/services/ListingService.js
+++ public/js/services/ListingService.js
@@ -2,14 +2,6 @@
     //var basicConfiguration = require('./basicConfiguration'); // use this one for testing
 
     var listingService = function($http){
-
-        //var getListing = function(street,buildingNumber,apartmentNumber){
-        //    return $http.get("http://localhost:3000/api/listing/"+street +"/"+buildingNumber+"/"+apartmentNumber)
-        //        .then(function(response){
-        //            return response.data;
-        //        });
-        //};
-
         var getListing = function(url){
             return $http.get("http://localhost:3000"+url)
                 .then(function(response){

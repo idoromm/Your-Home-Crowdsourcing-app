dd0227d45cc1c0a0e9e14a1c4680fdb724af1de3
julyay <julyay@wix.com>
2015-12-05 14:58:12 +0200

    modal and form

---
+++ public/js/directives/validationDirective.js
@@ -0,0 +1,18 @@
+app.directive('showErrors', function () {
+
+    return {
+        restrict: 'A',
+        require: '^form',
+        link: function (scope, el, attrs, formCtrl) {
+            var inputEl = el[0].querySelector("[name]");
+            var inputNgEl = angular.element(inputEl);
+            var inputName = inputNgEl.attr('name');
+            var helpText = angular.element(el[0].querySelector(".help-block"));
+
+            inputNgEl.bind('blur', function () {
+                el.toggleClass('has-error', formCtrl[inputName].$invalid);
+                helpText.toggleClass('hide', formCtrl[inputName].$valid);
+            });
+        }
+    }
+});

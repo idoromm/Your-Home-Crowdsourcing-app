58bc26cb48385143544456153d111d09f18b88d7
julyay <julyay@wix.com>
2015-12-15 13:24:48 +0200

    changed add review button to red plus icon and added it to the map, without the "jump"

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'ui.bootstrap']);
+var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngMdIcons']);
 
 app.config(function ($routeProvider) {
 	$routeProvider

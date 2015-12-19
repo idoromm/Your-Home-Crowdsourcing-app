06deb378202b9e921c103f0da4298d24f4550e92
julyay <julyay@wix.com>
2015-12-19 10:27:53 +0200

    cosmetic changes to see the json well, ctrl+alt+L

--- public/js/services/reviewService.js
+++ public/js/services/reviewService.js
@@ -1,11 +1,29 @@
 app.factory('reviewService',
     function ($http) {
 
-        var insertReview = function(listing,googleMapsAddress){
-            var data=JSON.stringify(({"latitude":googleMapsAddress.geometry.location.lat(),"longitude":googleMapsAddress.geometry.location.lng(),"country":googleMapsAddress.country,"city":googleMapsAddress.locality,"street":googleMapsAddress.route,"buildingNumber":googleMapsAddress.street_number,"apartmentNumber":listing.apartmentNumber,"type":listing.type,"floor":listing.floor,"outOfFloors":listing.outOfFloors,"size":listing.size,
-                "numberOfRooms":listing.rooms,"renovated":listing.renovated,"elevator":listing.elevator,"airConditioning":listing.airConditioning,"balcony":listing.balcony,"price":listing.price,"description":listing.description}));
-            return $http.post("/api/listing",data)
-                .then(function(response){
+        var insertReview = function (listing, googleMapsAddress) {
+            var data = JSON.stringify(({
+                "latitude": googleMapsAddress.geometry.location.lat(),
+                "longitude": googleMapsAddress.geometry.location.lng(),
+                "country": googleMapsAddress.country,
+                "city": googleMapsAddress.locality,
+                "street": googleMapsAddress.route,
+                "buildingNumber": googleMapsAddress.street_number,
+                "apartmentNumber": listing.apartmentNumber,
+                "type": listing.type,
+                "floor": listing.floor,
+                "outOfFloors": listing.outOfFloors,
+                "size": listing.size,
+                "numberOfRooms": listing.rooms,
+                "renovated": listing.renovated,
+                "elevator": listing.elevator,
+                "airConditioning": listing.airConditioning,
+                "balcony": listing.balcony,
+                "price": listing.price,
+                "description": listing.description
+            }));
+            return $http.post("/api/listing", data)
+                .then(function (response) {
                     return response.data;
                 });
         };

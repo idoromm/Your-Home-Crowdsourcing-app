1ba168c83602e73646fd2b9c5d82214fbb5db97c
Lior Gluskin <lior.gluskin@Vayyar.com>
2016-01-09 17:55:45 +0200

    register works now

--- app/routes.js
+++ app/routes.js
@@ -565,7 +565,7 @@ module.exports = function (app, passport) {
     });
 
 // process the signup form
-    app.post('/signup', passport.authenticate('local-signup', {
+     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/',			// redirect to the secure profile section
         failureRedirect: 'signup',		// redirect back to the signup page if there is an error
         failureFlash: true				// allow flash messages - shows messege for failure
